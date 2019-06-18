67436d2f48594c31808d44b4894e55617187beb6
Louis-Charles Martin <louischarles.martin@ticketmaster.com>
2019-06-18 17:33:51 -0400

    Update tests

--- ksql/client_test.go
+++ ksql/client_test.go
@@ -1,61 +1,162 @@
 package ksql
 
 import (
+	"fmt"
 	"log"
 	"strings"
 	"testing"
-	
+	"time"
+
 	kafka "github.com/Mongey/terraform-provider-kafka/kafka"
 )
 
+// CONFIG
+
+type testConfig struct {
+	ksqlServerHost string
+	kafkaBootstrapServers []string
+}
+
+var config = testConfig{
+	ksqlServerHost: "http://localhost:8088",
+	kafkaBootstrapServers: []string{"localhost:9092"},
+}
+
+// TESTS
+
+func TestNewClient(t *testing.T) {
+	_ = createNewClient(t)
+}
+
 func TestCreateTable(t *testing.T) {
-	c := NewClient("http://localhost:8088")
+	c := createNewClient(t)
 	defer deleteAllTablesAndStreams(t, c)
 }
 
-func TestCreateAndListAndDescribeStreams(t *testing.T) {
-	c := NewClient("http://localhost:8088")
+func TestCreateAndListAndDescribeStream(t *testing.T) {
+	c := createNewClient(t)
 	defer deleteAllTablesAndStreams(t, c)
 	
-	kc, err := kafka.NewClient(&kafka.Config{
-		BootstrapServers: &[]string{"localhost:9092"},
-		Timeout:          10,
-	})
-	
-	if err != nil {
-		t.Fatal(err)
-	}
-	
-	const topic = "users"
-	_ = createSimpleTopic(kc, topic)
-	
-	defer kc.DeleteTopic(topic)
-	
-	streamName := "userUpdates"
-	err = c.CreateStream(&CreateStreamRequest{
-		Name: streamName,
+	usersTopic := createSimpleTopic(t, "users")
+	defer usersTopic.Delete()
+
+	userUpdateStream := createTestStream(t, c, &CreateStreamRequest{
+		Name: "userUpdates",
 		Fields: map[string]string{
 			"gender": "string",
 		},
 		Settings: map[string]string{
-			"kafka_topic":  topic,
+			"kafka_topic":  usersTopic.Name,
 			"value_format": "JSON",
 		},
 	})
+	defer userUpdateStream.Drop()
 	
-	if err != nil {
-		t.Fatal(err)
-	}
-	
-	testListStreams(t, c, []string{streamName})
-	testDescribe(t, c, streamName)
+	testListStreams(t, c, []string{userUpdateStream.Name})
+	testDescribe(t, c, userUpdateStream.Name)
 }
 
-func TestListStreams(t *testing.T) {
-	c := NewClient("http://localhost:8088")
+func TestCreateAsSelectTerminateOnDrop(t *testing.T) {
+	c := createNewClient(t)
+
+	usersTopic := createSimpleTopic(t, "someTopic")
+	defer usersTopic.Delete()
+
+	someStream := createTestStream(t, c, &CreateStreamRequest{
+		Name: "someStream",
+		Fields: map[string]string{
+			"gender": "string",
+		},
+		Settings: map[string]string{
+			"kafka_topic":  usersTopic.Name,
+			"value_format": "JSON",
+		},
+	})
+
+	persistentStreamingQuery := &streamAsSelect{
+		stream: stream{
+			client: c,
+			CreateStreamRequest: CreateStreamRequest{
+				Name: "persistentQueryStream",
+			},
+		},
+		query: fmt.Sprintf("SELECT * FROM %s", someStream.Name),
+	}
+	err := persistentStreamingQuery.Create()
+
+	if err != nil {
+		t.Fatalf("unable to create stream '%s': %v", persistentStreamingQuery.Name, err)
+	}
+
+	testListStreams(t, c, []string{someStream.Name, persistentStreamingQuery.Name})
+
+	err = persistentStreamingQuery.Drop()
+	if err != nil {
+		t.Fatalf("unable to drop stream '%s': %v", persistentStreamingQuery.Name, err)
+	}
+
+	testListStreams(t, c, []string{someStream.Name})
+
+	err = someStream.Drop()
+	if err != nil {
+		t.Fatalf("unable to drop stream '%s': %v", someStream.Name, err)
+	}
+
+	testListStreams(t, c, []string{})
+}
+
+func TestListEmptyStreams(t *testing.T) {
+	c := createNewClient(t)
 	defer deleteAllTablesAndStreams(t, c)
 	testListStreams(t, c, []string{})
 }
+
+// SUB-TESTS (INPUTS REQUIRED)
+
+func testDescribe(t *testing.T, c *Client, name string) {
+	desc, err := c.Describe(name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectResourceName(t, name, desc.Name)
+
+	log.Printf("%v", desc)
+}
+
+func testListStreams(t *testing.T, c *Client, expectedStreams []string) {
+	streams, err := c.ListStreams()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Check for unexpected streams
+	for i, s := range streams {
+		found := false
+		for j, n := range expectedStreams {
+			if s.Name == strings.ToUpper(n) {
+				log.Printf("stream %d: %s", i, s.Name)
+				found = true
+				expectedStreams = removeFromSlice(expectedStreams, j)
+				break
+			}
+		}
+		if !found {
+			t.Errorf("unexpected stream: %s", s.Name)
+		}
+	}
+
+	// Check for missing streams
+	if len(expectedStreams) > 0 {
+		t.Errorf("missing expected streams: %v", expectedStreams)
+	}
+
+}
+
+// TEST ROUTINES
+// All functions belows should:
+//  - Receive 't *testing.T' in input
+//  - Log (t.Error) or fail (t.Fatal) if an unexpected error occures
 
 func deleteAllTablesAndStreams(t *testing.T, c *Client) {
 	tables, err := c.ListTables()
@@ -81,13 +182,96 @@ func deleteAllTablesAndStreams(t *testing.T, c *Client) {
 	}
 }
 
-func createSimpleTopic(kc *kafka.Client, topicName string) error {
-	return kc.CreateTopic(kafka.Topic{
+func createNewClient(t *testing.T) *Client {
+	host := config.ksqlServerHost
+	c := NewClient(host)
+	_, err := c.Info()
+	if err != nil {
+		t.Fatalf("unable to reach client at '%s': insure the KSQL server is up and reachable", host)
+	}
+	return c
+}
+
+func createNewKafkaClient(t *testing.T) *kafka.Client {
+	bootstrapServers := &config.kafkaBootstrapServers
+	kc, err := kafka.NewClient(&kafka.Config{
+		BootstrapServers: bootstrapServers,
+		Timeout:          10,
+	})
+	if err != nil {
+		t.Fatalf("unable to create kafka client at '%v': %v", &config.kafkaBootstrapServers, err)
+	}
+	return kc
+}
+
+type topic struct {
+	Name string
+	kc   *kafka.Client
+}
+
+func (t *topic) Delete() error {
+	return t.kc.DeleteTopic(t.Name)
+}
+
+func createSimpleTopic(t *testing.T, topicName string) *topic {
+	kc := createNewKafkaClient(t)
+	err := kc.CreateTopic(kafka.Topic{
 		Name:              topicName,
 		Partitions:        1,
 		ReplicationFactor: 1,
 	})
+	topic := &topic{
+		kc: kc,
+		Name: topicName,
+	}
+	if err != nil {
+		t.Fatalf("unable to create topic '%s': %v", topic.Name, err)
+	}
+	return topic
 }
+
+type stream struct {
+	CreateStreamRequest
+	client *Client
+}
+
+func (s *stream) Drop() error {
+	return s.client.DropStream(&DropStreamRequest{Name: s.Name})
+}
+
+func (s *stream) Exists() bool {
+	desc, _ := s.client.Describe(s.Name)
+	if desc == nil {
+		return false
+	}
+	return true
+}
+
+func createTestStream(t *testing.T, c *Client, req *CreateStreamRequest) *stream {
+	err := c.CreateStream(req)
+	if err != nil {
+		t.Fatalf("unable to create stream '%s': %v", req.Name, err)
+	}
+
+	s := &stream{*req, c}
+	i := 0
+
+	for {
+		if s.Exists() {
+			break
+		}
+		if i > 10 {
+			t.Fatalf("unable to create stream '%s': %v", s.Name, err)
+		}
+		log.Printf("[DEBUG] waiting on stream '%s' creation", s.Name)
+		time.Sleep(1 * time.Second)
+		i++
+	}
+
+	return s
+}
+
+// ASSERTIONS
 
 func expectResourceName(t *testing.T, expected string, real string) {
 	expected = strings.ToUpper(expected)
@@ -96,44 +280,19 @@ func expectResourceName(t *testing.T, expected string, real string) {
 	}
 }
 
-func testDescribe(t *testing.T, c *Client, name string) {
-	desc, err := c.Describe(name)
-	if err != nil {
-		t.Fatal(err)
-	}
+// HELPERS
 
-	expectResourceName(t, name, desc.Name)
-
-	log.Printf("%v", desc)
+type streamAsSelect struct {
+	stream
+	query string
 }
 
-func testListStreams(t *testing.T, c *Client, expectedStreams []string) {
-	streams, err := c.ListStreams()
-	if err != nil {
-		t.Fatal(err)
-	}
+func (s *streamAsSelect) Create() error {
+	q := fmt.Sprintf("CREATE STREAM %s AS %s;", s.Name, s.query)
+	log.Printf("[WARN] Query %s", q)
 
-	// Check for unexpected streams
-	for i, s := range streams {
-		found := false
-		for j, n := range expectedStreams {
-			if s.Name == strings.ToUpper(n) {
-				log.Printf("Stream %d: %s", i, s.Name)
-				found = true
-				expectedStreams = removeFromSlice(expectedStreams, j)
-				break
-			}
-		}
-		if !found {
-			t.Errorf("Unexpected stream: %s", s.Name)
-		}
-	}
-
-	// Check for missing streams
-	if len(expectedStreams) > 0 {
-		t.Errorf("Missing expected streams: %v", expectedStreams)
-	}
-
+	_, err := s.client.Do(Request{KSQL: q})
+	return err
 }
 
 // removeFromSlice removes a specified index from a slice.
